794ebba5abcc3dc5d99221148e878a8c531f792d
popovaludmila <57451404+popovaludmila@users.noreply.github.com>
2020-05-08 15:01:09 +0300

    Мобильное меню при отключенном JS

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,7 @@ var navToggle = document.querySelector('.main-nav__toggle');
 var headerInner = document.querySelector('.page-header__inner');
 
 navMain.classList.remove('main-nav--nojs');
+headerInner.classList.remove('page-header__inner--opened');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
