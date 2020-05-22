bb39d6aa3aaa2e5be2f3d1f4d874cb876aabf99a
popovaludmila <57451404+popovaludmila@users.noreply.github.com>
2020-05-22 14:44:23 +0300

    Испправление шрифтов

--- gulpfile.js
+++ gulpfile.js
@@ -31,6 +31,7 @@ gulp.task("css", function () {
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"));
+    .pipe(server.stream());
 });
 
 gulp.task("refresh", function (done) {
